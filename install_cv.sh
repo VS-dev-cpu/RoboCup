f4df8bf6892d67e7427f776ed7d049cd75bf6889
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-12 10:32:22 +0200

    Create install_cv.sh

---
+++ install_cv.sh
@@ -0,0 +1,10 @@
+echo "Installing Python Libs for OpenCV"
+
+pip3 install opencv-python
+pip3 install opencv-contrib-python
+
+pip3 install numpy
+
+echo "Installing Libs for Serial Communication"
+
+pip3 install pyserial

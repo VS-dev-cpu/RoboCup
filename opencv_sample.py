77c073214aec5e68703778afe6418b34acf9af8a
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-14 13:14:30 +0200

    Update opencv_sample.py

--- opencv_sample.py
+++ opencv_sample.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 init = False
 port = '/dev/ttyACM0'
@@ -18,10 +19,17 @@ import numpy as np
 W = 128
 H = W/16*9
 
+IMAGE_FLIP_HORIZONTALLY = True
+IMAGE_RESIZE = True
+
 cap = cv2.VideoCapture(0)
-cap.set(3, W)
-cap.set(4, H)
+if (IMAGE_RESIZE):
+    cap.set(3, W)
+    cap.set(4, H)
 _, frame = cap.read()
+frame = cv2.flip(frame, 0)
+if (IMAGE_FLIP_HORIZONTALLY):
+    frame = cv2.flip(frame, 1)
 rows, cols, _ = frame.shape
 
 ballX = 0
@@ -31,20 +39,28 @@ ballDist = 0
 
 debugging = True
 
+ser.write(b'-1\n')
+time.sleep(2)
+
 while True:
     _, frame = cap.read()
+    frame = cv2.flip(frame, 0)
+    if (IMAGE_FLIP_HORIZONTALLY):
+        frame = cv2.flip(frame, 1)
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     low_red = np.array([161, 155, 84])
     high_red = np.array([179, 255, 255])
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
-    _, contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
 
     ballX = W / 2
     ballY = H / 2
     ballSize = 0
     ballDist = 0
+    
+    ball = False
 
     for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
@@ -53,23 +69,30 @@ while True:
         ballY = int((y + y + h) / 2)
         ballSize = int((w + h) / 2)
         ballDist = int(-ballSize + w)
+        ball = True
         if (debugging):
           print(str(ballX) + ', ' + str(ballY) + ', ' + 
           str(ballSize) + ', ' + str(ballDist) + '\n')
         break
 
-    if (init):
-      ser.write(str(ballX) + ',' + str(ballY) + ',')
-      ser.write(str(ballSize) + ',' + str(ballDist))
-      ser.write(b'\n')
+    if (ball):
+      if (ballX < (W/2 - W/2)):
+        ser.write(b'2')
+      elif (ballX > (W/2 + W/2)):
+          ser.write(b'1')
+      else:
+          ser.write(b'3')
+    else:
+        ser.write(b'2')
     
     if(debugging):
-      cv2.line(frame, (ballX, 0), (ballX, W), (0, 255, 0), 2)
-      cv2.line(frame, (ballY, 0), (ballY, H), (0, 255, 0), 2)
+      frame = cv2.line(frame, (int(ballX), 0), (int(ballX), int(H)*10), (0, 255, 0), 2)
+      frame = cv2.line(frame, (0, int(ballY)), (int(W)*10, int(ballY)), (0, 255, 0), 2)
     
       cv2.imshow("Frame", frame)
       
     key = cv2.waitKey(1)
+    ser.write(b'\n')
     
     if key == 27:
         break

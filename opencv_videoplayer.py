afa45e89b4b1558a79734d97ed5ecd40f145b4fb
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-18 19:50:32 +0200

    Update opencv_videoplayer.py

--- opencv_videoplayer.py
+++ opencv_videoplayer.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
-from playsound import playsound
+import os
+
+from datetime import datetime
+import calendar
+
+def get_unix():
+    d = datetime.utcnow()
+    return calendar.timegm(d.utctimetuple())
 
 current = ""
 cap = cv2.VideoCapture("")
@@ -11,24 +18,40 @@ def load(name):
     current = name
     cap.release()
     cap = cv2.VideoCapture(str(name) + '.mkv')
-    if (cap.isOpened()== False):
-        cap = cv2.VideoCapture(str(name) + '_backup.mkv')
+    
+def playsound(path):
+    os.system("mpg123 " +  str(path) + " &")
       
 def play():
-    playsound(str(current) + '.wav')
-    while(cap.isOpened()):
+    playsound(str(current) + '.mp3')
+    ret = True
+    while(ret):
         ret, frame = cap.read()
         if ret == True:
           cv2.imshow('window', frame)
+        
+def playForTime(secs):
+    start = get_unix()
+    playsound(str(current) + '.mp3')
+    ret = True
+    while (ret):
+        ret, frame = cap.read()
+        if ret == True:
+          cv2.imshow('window', frame)
+        if (get_unix() - start >= secs)
+            ret = False
+            
+load("info")
+playForTime(226)
     
 load("sunrise")
-play()
+playForTime(19)
 
 load("searching")
 play()
 
 load("dance")
-play()
+playForTime()
     
 
 cv2.destroyAllWindows()

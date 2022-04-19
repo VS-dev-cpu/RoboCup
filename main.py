72d1611b83d68758c616bc85405ecdeac5b43abe
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-19 16:30:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,6 @@ rows, cols, _ = frame.shape
 low_acorn = np.array([161, 155, 84])
 high_acorn = np.array([179, 255, 255])
 
-low_wall= np.array([80,50,50])
-high_wall= np.array([100,255,255])
-
 W = cap.get(3)
 H = cap.get(4)
 
@@ -56,14 +53,7 @@ acornDist = 0
 gap = 2.5
 minacornSize = 150
 
-wall = False
-wallX = 0
-wallY = 0
-wallSize = 0
-wallDist = 0
-minWallSize = 400
-
-debugging = True
+debugging = False
 
 bt = bt.BT("basic")
 
@@ -95,23 +85,13 @@ while True:
     acorn_mask = cv2.inRange(hsv_frame, low_acorn, high_acorn)
     acorn_contours, _ = cv2.findContours(acorn_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     acorn_contours = sorted(acorn_contours, key=lambda x:cv2.contourArea(x), reverse=True)
-    
-    wall_mask = cv2.inRange(hsv_frame, low_wall, high_wall)
-    wall_contours, _ = cv2.findContours(wall_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    wall_contours = sorted(wall_contours, key=lambda x:cv2.contourArea(x), reverse=True)
-    
+   
     acornX = W / 2
     acornY = H / 2
     acornSize = 0
     acornDist = 0
     acorn = False
     
-    wallX = W / 2
-    wallY = H / 2
-    wallSize = 0
-    wallDist = 0
-    wall = False
-
     for cnt in acorn_contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
         
@@ -120,16 +100,6 @@ while True:
         acornSize = int((w + h) / 2)
         acornDist = int(-acornSize + w)
         acorn = True
-        break
-    
-    for cnt in wall_contours:
-        (x, y, w, h) = cv2.boundingRect(cnt)
-        
-        wallX = int((x + x + w) / 2)
-        wallY = int((y + y + h) / 2)
-        wallSize = int((w + h) / 2)
-        wallDist = int(-wallSize + w)
-        wall = True
         break
     
     # Move the robot
@@ -142,23 +112,11 @@ while True:
           ser.write(b'1')
       else:
           ser.write(b'3')
+    else:
+	ser.write(b'1')
 
-    if (wall):
-      if (wallSize > minWallSize):
-          ser.write(b'-3')
-      elif (wallX < (W/2 - W/gap)):
-        ser.write(b'2')
-      elif (wallX > (W/2 + W/gap)):
-          ser.write(b'1')
-      else:
-          ser.write(b'3')
-    elif not acorn:
-    	ser.write(b'1')
-      
     ser.write(b'\n')
-    if wall and wallSize > minWallSize:
-        time.sleep(6)
-      
+
     key = cv2.waitKey(1)
     
     # Exit, if needed

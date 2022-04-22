dd3b16e1c0e993a28441c37a340aa3c7132e029d
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-22 16:06:30 +0200

    Update and rename install_cv.sh to install_mediatools.sh

--- install_mediatools.sh
+++ install_mediatools.sh
@@ -5,12 +5,11 @@ pip3 install --upgrade pip
 
 pip3 install opencv-contrib-python
 
-pip3 install numpy
-
 echo "Installing Libs for Serial Communication"
 
 pip3 install pyserial
 
-echo "Installing Libs for Sound Playing"
+echo "Installing Libs for Video Playing"
 
-pip3 install mpg123
+pip3 install pygame
+pip3 install moviepy
